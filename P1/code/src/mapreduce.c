bfd0e64e5a405c26103d1e06a7a36a889797ab4b
kaupau <kaushikpa@gmail.com>
2021-02-17 14:54:43 -0600

    interim

--- P1/code/src/mapreduce.c
+++ P1/code/src/mapreduce.c
@@ -1,5 +1,23 @@
 #include "mapreduce.h"
 
+void spawnMapper(int nMappers) {
+	int process;
+	for(int i=1; i<=nMappers; i++) {
+		process = fork();
+		if(process == 0)
+			execlp("mapper.exe", "mapper.exe", i, nMappers, NULL);
+	}
+}
+
+void spawnReducers(int nReducers) {
+	int process;
+	for(int i=1; i<=nReducers; i++) {
+		process = fork();
+		if(process == 0)
+			execlp("reducer.exe", "reducer.exe", i, nReducers, NULL);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	
 	//TODO: number of argument check
@@ -14,9 +32,7 @@ int main(int argc, char *argv[]) {
 	bookeepingCode();
 
 	// TODO: spawn mappers	
-	int process = fork();
-	if(process == 0)
-		execlp("mapper.exe", "mapper.exe", nMappers, NULL);
+	spawnMapper(nMappers);
 
 	// TODO: wait for all children to complete execution
 	for(int i=0; i<nMappers; i++) {
@@ -24,9 +40,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	// TODO: spawn reducers
-	process = fork();
-	if(process == 0)
-		execlp("reducer.exe", "reducer.exe", nReducers, NULL);
+	spawnReducers(nReducers);
 
 	// TODO: wait for all children to complete execution
 	for(int i=0; i<nMappers; i++) {
