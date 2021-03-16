cac753ae889ef8e4588402400037bd787b3e1fc4
Alan Wang <wang7200@umn.edu>
2021-03-15 19:03:19 -0500

    Completed MapperInput creation

--- P2/code/src/mapreduce.c
+++ P2/code/src/mapreduce.c
@@ -2,6 +2,10 @@
 
 char** getTxtFiles(char* path, char** txtFileBuffer);
 
+int isTxtFile(char* filename);
+
+char* constructPath(char* pathto, char* filename);
+
 int main(int argc, char *argv[]) {
 
 	//TODO: number of argument check
@@ -19,12 +23,35 @@ int main(int argc, char *argv[]) {
 	// and create MapperInput directory which will contain MapperID.txt files
 	char* txtFiles[MaxNumInputFiles];
 	getTxtFiles(inputFileDir, txtFiles);
-	printf("\n\nFinal array of txt files:\n");
-	char** txtname = txtFiles;
-	while(*txtname != NULL) {
-		printf("\t%s\n",*txtname);
-		txtname++;
+
+	if(mkdir("MapperInput",0777) < 0) {
+		printf("Making MapperInput folder failed, try make clean\n");
 	}
+
+	FILE** mapperIDtxtfiles = malloc(nMappers * sizeof(FILE*));
+	char* filepath;
+	char filename[maxFileNameLength];
+	for(int i = 0; i < nMappers; i++) {
+		sprintf(filename, "Mapper_%d.txt",i+1);
+		filepath = constructPath("MapperInput",filename);
+		mapperIDtxtfiles[i] = fopen(filepath,"w");
+		free(filepath);
+	}
+
+	printf("\nWriting to Mapper_xx.txt files...\n");
+	int mapperID = 0;
+	for(char** txtname = txtFiles; *txtname != NULL; txtname++) {
+		printf("\t%s assigned to mapper mapperID %d\n",*txtname, mapperID);
+		fprintf(mapperIDtxtfiles[mapperID],"%s\n",*txtname);
+		free(*txtname);
+		mapperID = (mapperID + 1)%nMappers;
+	}
+
+	for(int i = 0; i < nMappers; i++) {
+		fclose(mapperIDtxtfiles[i]);
+	}
+	free(mapperIDtxtfiles);
+
 	printf("Done\n\n");
 	//TODO: spawn stream processes
 
@@ -39,9 +66,6 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
-int isTxtFile(char* filename);
-
-char* constructPath(char* pathto, char* filename);
 
 char** getTxtFiles(char* path, char** txtFileBuffer) {
 	DIR* dr = opendir(path);
@@ -53,7 +77,7 @@ char** getTxtFiles(char* path, char** txtFileBuffer) {
 	struct dirent* de;
 	while((de = readdir(dr)) != NULL) {
 		if(de->d_type == DT_DIR) {
-			printf("Directory: %s\n",de->d_name);
+			printf("\tDirectory: %s\n",de->d_name);
 			if(de->d_name[0]=='.') {
 				if(strlen(de->d_name)==1) { continue; }
 				if(strlen(de->d_name)==2 && de->d_name[1]=='.') { continue; }
@@ -62,15 +86,15 @@ char** getTxtFiles(char* path, char** txtFileBuffer) {
 			txtFileBuffer = getTxtFiles(newpath, txtFileBuffer);
 			free(newpath);
 		} else {
-			printf("File: %s\n",de->d_name);
+			printf("\tFile: %s\n",de->d_name);
 			if(isTxtFile(de->d_name)) {
-				printf("Txt file found\n");
+				printf("\tTxt file found\n");
 				*txtFileBuffer = malloc(maxFileNameLength * sizeof(char));
 				(*txtFileBuffer)[0] = '\0';
 				char* newpath = constructPath(path,de->d_name);
 				strcat(*txtFileBuffer,newpath);
 				free(newpath);
-				printf("Saved filename: %s\n",*txtFileBuffer);
+				printf("\tSaved filename: %s\n",*txtFileBuffer);
 				txtFileBuffer++;
 			}
 		}
