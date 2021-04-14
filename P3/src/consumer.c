48a5b96c01010062cb9c05b8f597607c9ef682ef
kaupau <kaushikpa@gmail.com>
2021-04-14 14:36:44 -0500

    rearrange push pop

--- P3/src/consumer.c
+++ P3/src/consumer.c
@@ -1,18 +1,5 @@
 #include "consumer.h"
 
-char* pop(void* arg) {
-    struct sharedBuffer* buffer = (struct sharedBuffer*) arg;
-    pthread_mutex_lock(buffer->mutex);
-    
-    struct node *current = buffer->head;
-    buffer->head = buffer->head->next;
-    char *line = current->line;
-    free(current);
-
-    pthread_mutex_unlock(buffer->mutex);
-    return line;
-}
-
 /**
  * parse lines from the queue, and count words by word length
  */
@@ -25,9 +12,18 @@ void *consumer(int consumerID, void *arg){
     printf("consumer %d\n", consumerID);
     //TODO: keep reading from queue and process the data
     // feel free to change
+    struct sharedBuffer* buffer = (struct sharedBuffer*) arg;
+
     while(1){
-        printf("consumer %d: %d\n");
-        pop(arg);
+        printf("consumer %d: %d\n", consumerID);
+        
+        pthread_mutex_lock(buffer->mutex);
+        struct node *current = buffer->head;
+        buffer->head = buffer->head->next;
+        char *line = current->line;
+        free(current);
+        pthread_mutex_unlock(buffer->mutex);
+
         parse();
     }
     
--- P3/src/producer.c
+++ P3/src/producer.c
@@ -1,17 +1,5 @@
 #include "producer.h"
 
-void push(char* line, void* arg) {
-    struct sharedBuffer* buffer = (struct sharedBuffer*) arg;
-    pthread_mutex_lock(buffer->mutex);
-    struct node *current = buffer->head;
-    while(current->next != NULL) {
-        current = current->next;
-    }
-    current->next = malloc(sizeof(struct node));
-    *(current->next) = (struct node) {NULL, line};
-    pthread_mutex_unlock(buffer->mutex);
-}
-
 /**
  *
  * Producer thread will read from the file and write data to 
@@ -23,14 +11,23 @@ void *producer(char* filename, void *arg){
     //Send data to the shared queue
     //When reaching the end of the file, send EOF message to the shared queue
     FILE *inputFile = getFilePointer(filename);
+    struct sharedBuffer* buffer = (struct sharedBuffer*) arg;
+    int lineNumber = 0;
 
     char line[maxFileNameLength];
     while( getLineFromFile(inputFile, line, maxFileNameLength) != -1 ) {
-        printf("producer: %d\n");
+        printf("producer: %d\n", lineNumber++);
+
         line[strlen(line)-1] = '\0';
         
-        
-        push(line, arg);
+        pthread_mutex_lock(buffer->mutex);
+        struct node *current = buffer->head;
+        while(current->next != NULL) {
+            current = current->next;
+        }
+        current->next = malloc(sizeof(struct node));
+        *(current->next) = (struct node) {NULL, line};
+        pthread_mutex_unlock(buffer->mutex);
     }
 
     pthread_cond_broadcast(buffer->EOFSignal);
